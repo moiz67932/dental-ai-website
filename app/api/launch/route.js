f7a6d0e763f4c2d837e542b6cfae0254c76c11ef
Muhammad Moiz <moiz@67932@gmail.com>
2025-06-29 22:31:29 +0500

    first commit

--- app/api/launch/route.js
+++ app/api/launch/route.js
@@ -43,7 +43,7 @@ import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
 const supa = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY // **service-role** key, NOT anon
 );
 
